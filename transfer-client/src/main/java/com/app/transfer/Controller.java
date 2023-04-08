d7b8cc50db7f8cb58f29e62c43a7eaf5366ec90f
suhaib <sohaib_salah96@yahoo.com>
2023-04-08 07:14:23 +0000

    create table to render the data using JDBC to contact with database

--- transfer-client/src/main/java/com/app/transfer/Controller.java
+++ transfer-client/src/main/java/com/app/transfer/Controller.java
@@ -4,10 +4,15 @@ import com.jk.web.faces.controllers.JKWebController;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
 
+import java.util.List;
+import java.util.Vector;
+
 @Named("controller")
 @ViewScoped
 public class Controller extends JKWebController {
     Model model;
+    List<Model> list;
+    List<Model> filterLest;
     ServiceClient serviceClient = new ServiceClient();
 
 
@@ -17,15 +22,33 @@ public class Controller extends JKWebController {
 
     public String add(){
         serviceClient.insert(model);
+        refresh();
         success("Added Successfully");
         return null;
     }
-
-
+    public void setModel(Model model){
+        this.model = model;
+    }
     public Model getModel(){
         if(model == null){
             model = new Model();
         }
         return model;
+    }
+    public List<Model>getList(){
+        if(list == null){
+            list = serviceClient.getAll();
+        }
+        return list;
+    }
+    public List<Model>getFilterLest(){
+        return filterLest;
+    }
+    public void setFilterLest(Vector<Model> filterLest){
+        this.filterLest = filterLest;
+    }
+    protected void refresh(){
+        this.list = null;
+        setModel(model);
     }
 }
