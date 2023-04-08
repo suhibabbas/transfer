92d90465e0dcd8ad7f07ec6503d5b8b990332217
suhaib <sohaib_salah96@yahoo.com>
2023-04-08 09:26:26 +0000

    added line chart that shows analysis for the transefer amount

--- transfer-client/src/main/java/com/app/transfer/Controller.java
+++ transfer-client/src/main/java/com/app/transfer/Controller.java
@@ -1,8 +1,13 @@
 package com.app.transfer;
 
 import com.jk.web.faces.controllers.JKWebController;
+import jakarta.annotation.PostConstruct;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.LineChartSeries;
 
 import java.util.List;
 import java.util.Vector;
@@ -50,5 +55,46 @@ public class Controller extends JKWebController {
     protected void refresh(){
         this.list = null;
         setModel(model);
+    }
+
+    ServiceClient sClient = new ServiceClient();
+
+
+    private LineChartModel lineModel;
+
+
+
+    @PostConstruct
+    public void init() {
+        createLineModels();
+    }
+
+    public LineChartModel getLineModel() {
+        return lineModel;
+    }
+
+
+    private LineChartModel initLineChart() {
+        LineChartModel model = new LineChartModel();
+        LineChartSeries series = new LineChartSeries();
+        series.setLabel("amount");
+
+        for (Model m : sClient.getAll()) {
+            series.set(m.getId(), m.getAmount());
+
+        }
+        model.addSeries(series);
+
+        return model;
+    }
+
+    private void createLineModels(){
+        lineModel = initLineChart();
+        lineModel.setTitle("Transfer");
+        lineModel.setLegendPosition("e");
+        Axis yAxis = lineModel.getAxis(AxisType.Y);
+        yAxis.setMin(0);
+        yAxis.setMax(500);
+
     }
 }
